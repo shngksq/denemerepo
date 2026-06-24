8de928047f58d3c4cc47735f9e331feac9caf66d
shngksq <barlasdeniz8@gmail.com>
2026-06-24 05:27:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,25 @@ app.get("/", (req, res) => {
   res.send("LikeSoar Bot Çalışıyor");
 });
 
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = "likesoar123";
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  }
+
+  res.sendStatus(403);
+});
+
+app.post("/webhook", (req, res) => {
+  console.log(JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
